2bc96f148e0fd03a79c4abc5efb4ba17d622ac96
Matthieu Le Guellaut <matthieu.leguellaut@viacesi.fr>
2017-08-24 12:14:04 +0200

    Small potato fix

--- verbose.go
+++ verbose.go
@@ -52,7 +52,7 @@ func log(msg string){
 		f,err:=os.OpenFile(filename, os.O_APPEND|os.O_WRONLY,0600)
 		defer f.Close()
 		if err!=nil{ panic(err)}
-		_,err=f.WriteString(msg)
+		_,err=f.WriteString(msg+"\n")
 		if err!=nil{ panic(err)}
 	}
 
